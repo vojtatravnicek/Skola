b46c2dab349c2bdd40175e064acf00662142e4be
vojtatravnicek <vojta.travnicek@seznam.cz>
2018-11-14 13:56:57 +0100

    Dokoncen program; pocita kombinacni cislo; pridana chybova hlaska

--- Lekce07/KombinacniCislo.java
+++ Lekce07/KombinacniCislo.java
@@ -1,12 +1,20 @@
 public class KombinacniCislo {
 	public static void main(String[] args) {
-		long cislo = Integer.parseInt(args[0]);
+		long n = Integer.parseInt(args[0]);
+		long k = Integer.parseInt(args[1]);
 		
-		long faktorial = PocitaFaktorial(cislo);
+		if (k > n) {
+			System.out.printf ("Nemuze byt vybrano %d prvku z mnoziny o %d prvcich.", k, n) ;
+		} else {
+		long faktorial = PocitaFaktorial(n) / (PocitaFaktorial(n-k) * PocitaFaktorial(k)) ;
+		
 		
 		System.out.printf("%d", faktorial);
+		}
 	
 	}
+	
+	
 	
 	private static long PocitaFaktorial (long cislo) {
 		long faktorial = 1 ;
